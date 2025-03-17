ceae6ff18ecdd3fa3426ebbb5c79f15f8cc7c9c7
한성길 <zaien24@naver.com>
2025-03-17 12:38:54 +0900

    first commit

---
+++ src/test/java/com/tae/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tae.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
