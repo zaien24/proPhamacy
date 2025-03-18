dcf68b1e6b36069ccc42cae440484fee663ca699
한성길 <zaien24@naver.com>
2025-03-18 11:24:23 +0900

    서비스 구현

--- src/main/java/com/tae/project/api/dto/KakaoApiResponseDto.java
+++ src/main/java/com/tae/project/api/dto/KakaoApiResponseDto.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class KakaoAPiResponseDto {
+public class KakaoApiResponseDto {
 
     @JsonProperty("meta")
     private MetaDto metaDto;
---
+++ src/main/java/com/tae/project/api/service/KakaoAddressSearchService.java
@@ -0,0 +1,43 @@
+package com.tae.project.api.service;
+
+import com.tae.project.api.dto.KakaoApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KakaoAddressSearchService {
+
+    private final KakaoUriBuilderService kakaoUriBuilderService;
+
+    private final RestTemplate restTemplate;
+
+    @Value("${kakao.rest.api.key}")
+    private String kakaoRestApiKey;
+
+    public KakaoApiResponseDto requestAddressSearch(String address) {
+
+        if(ObjectUtils.isEmpty(address)) return null;
+
+        URI uri = kakaoUriBuilderService.buildUriByAddressSearch(address);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoRestApiKey);
+        HttpEntity httpEntity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
+    }
+}
