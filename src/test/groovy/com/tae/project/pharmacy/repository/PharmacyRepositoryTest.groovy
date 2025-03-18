794edec06e81f802332a5ab082d6d5104e906559
한성길 <zaien24@naver.com>
2025-03-18 15:05:39 +0900

    09.JPA Auditing으로 생성시간 수정 시간 자동화 구현

--- src/test/groovy/com/tae/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/tae/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -3,6 +3,7 @@ package com.tae.project.pharmacy.repository
 import com.tae.project.AbstractIntegrationContainerBaseTest
 import com.tae.project.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
+import java.time.LocalDateTime
 
 
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
@@ -58,6 +59,27 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         then:
         result.size() == 1
 
+    }
+
+    def "BaseTimeEntity_등록"() {
+
+        given:
+        def now = LocalDateTime.now()
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
     }
 
 }
