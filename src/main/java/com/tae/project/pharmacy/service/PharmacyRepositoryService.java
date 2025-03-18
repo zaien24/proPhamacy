b61fb4145b986be6774f2971f475a83cf8ccae9f
한성길 <zaien24@naver.com>
2025-03-18 14:40:04 +0900

    dirty checking 차이 알아보기

--- src/main/java/com/tae/project/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/tae/project/pharmacy/service/PharmacyRepositoryService.java
@@ -37,4 +37,5 @@ public class PharmacyRepositoryService {
         entity.changePharmacyAddress(address);
     }
 
+
 }
--- src/test/groovy/com/tae/project/pharmacy/service/PharmacyRepositoryServiceTest.groovy
+++ src/test/groovy/com/tae/project/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -1,9 +1,9 @@
 package com.tae.project.pharmacy.service
 
 import com.tae.project.AbstractIntegrationContainerBaseTest
+import com.tae.project.pharmacy.entity.Pharmacy
 import com.tae.project.pharmacy.repository.PharmacyRepository
 import org.springframework.beans.factory.annotation.Autowired
-import spock.lang.Specification
 
 class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest {
 
@@ -38,4 +38,27 @@ class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest
         then:
         result.get(0).getPharmacyAddress() == modifiedAddress
     }
+
+    def "PharmacyRepository update - dirty checking fail"() {
+
+        given:
+        String inputAddress = "서울 특별시 성북구 종암동"
+        String modifiedAddress = "서울 광진구 구의동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(inputAddress)
+                .pharmacyName(name)
+                .build()
+        when:
+        def entity = pharmacyRepository.save(pharmacy)
+        pharmacyRepositoryService.updateAddressWithoutTransaction(entity.getId(), modifiedAddress)
+
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getPharmacyAddress() == inputAddress
+    }
+
+
 }
