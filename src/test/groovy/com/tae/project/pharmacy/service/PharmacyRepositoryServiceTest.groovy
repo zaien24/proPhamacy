30885e07c92b7d5f2f96e1c7c3eec891aa5a8bfd
한성길 <zaien24@naver.com>
2025-03-18 14:26:38 +0900

    주소 업데이트 dirty 체킹 서비스 및 테스트 코드

---
+++ src/test/groovy/com/tae/project/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -0,0 +1,41 @@
+package com.tae.project.pharmacy.service
+
+import com.tae.project.AbstractIntegrationContainerBaseTest
+import com.tae.project.pharmacy.repository.PharmacyRepository
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Specification
+
+class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    private PharmacyRepositoryService pharmacyRepositoryService
+
+    @Autowired
+    PharmacyRepository pharmacyRepository
+
+    void setup() {
+        pharmacyRepository.deleteAll()
+    }
+
+    def "PharmacyRepository update - dirty checking success"() {
+
+        given:
+        String inputAddress = "서울 특별시 성북구 종암동"
+        String modifiedAddress = "서울 광진구 구의동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(inputAddress)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        def entity = pharmacyRepository.save(pharmacy)
+        pharmacyRepositoryService.updateAddress(entity.getId(), modifiedAddress)
+
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getPharmacyAddress() == modifiedAddress
+    }
+}
