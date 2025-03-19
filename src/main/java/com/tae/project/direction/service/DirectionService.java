ba78ea0981e98831080898cdfd78334899a4b5f2
한성길 <zaien24@naver.com>
2025-03-19 15:27:27 +0900

    카카오 키워드 장소 검색 api 적용하기

--- src/main/java/com/tae/project/direction/service/DirectionService.java
+++ src/main/java/com/tae/project/direction/service/DirectionService.java
@@ -1,6 +1,7 @@
 package com.tae.project.direction.service;
 
 
+import com.tae.project.api.service.KakaoCategorySearchService;
 import com.tae.project.direction.repository.DirectionRepository;
 import com.tae.project.pharmacy.service.PharmacySearchService;
 import lombok.RequiredArgsConstructor;
@@ -29,6 +30,7 @@ public class DirectionService {
 
     private final PharmacySearchService pharmacySearchService;
     private final DirectionRepository directionRepository;
+    private final KakaoCategorySearchService kakaoCategorySearchService;
 
     @Transactional
     public List<Direction> saveAll(List<Direction> directionList) {
@@ -55,6 +57,28 @@ public class DirectionService {
                                 .build())
                 .filter(direction -> direction.getDistance() <= RADIUS_KM)
                 .sorted(Comparator.comparing(Direction::getDistance))
+                .limit(MAX_SEARCH_COUNT)
+                .collect(Collectors.toList());
+    }
+
+    // pharmacy search by category kakao api
+    public List<Direction> buildDirectionListByCategoryApi(DocumentDto inputDocumentDto) {
+        if(Objects.isNull(inputDocumentDto)) return Collections.emptyList();
+
+        return kakaoCategorySearchService
+                .requestPharmacyCategorySearch(inputDocumentDto.getLatitude(), inputDocumentDto.getLongitude(), RADIUS_KM)
+                .getDocumentList()
+                .stream().map(resultDocumentDto ->
+                        Direction.builder()
+                                .inputAddress(inputDocumentDto.getAddressName())
+                                .inputLatitude(inputDocumentDto.getLatitude())
+                                .inputLongitude(inputDocumentDto.getLongitude())
+                                .targetPharmacyName(resultDocumentDto.getPlaceName())
+                                .targetAddress(resultDocumentDto.getAddressName())
+                                .targetLatitude(resultDocumentDto.getLatitude())
+                                .targetLongitude(resultDocumentDto.getLongitude())
+                                .distance(resultDocumentDto.getDistance() * 0.001) // km 단위
+                                .build())
                 .limit(MAX_SEARCH_COUNT)
                 .collect(Collectors.toList());
     }
--- src/main/java/com/tae/project/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/tae/project/pharmacy/service/PharmacyRecommendationService.java
@@ -34,6 +34,7 @@ public class PharmacyRecommendationService {
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
 
         List<Direction> directionList = directionService.buildDirectionList(documentDto);
+        List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
 
         directionService.saveAll(directionList);
 
