b2b2ef35e4381c2aaf583c295d1b66fd637de855
한성길 <zaien24@naver.com>
2025-03-19 14:15:38 +0900

    반경 10km 및 약국 3개 검색 기능 구현

---
+++ src/main/java/com/tae/project/direction/service/DirectionService.java
@@ -0,0 +1,62 @@
+package com.tae.project.direction.service;
+
+
+import com.tae.project.pharmacy.service.PharmacySearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import com.tae.project.api.dto.DocumentDto;
+import com.tae.project.direction.entity.Direction;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DirectionService {
+
+    private static final int MAX_SEARCH_COUNT = 3; // 최대 검색 갯수
+    private static final double RADIUS_KM = 10.0; // 반경 10 km
+
+    private final PharmacySearchService pharmacySearchService;
+
+    public List<Direction> buildDirectionList(DocumentDto documentDto) {
+        if(Objects.isNull(documentDto)) return Collections.emptyList();
+
+        return pharmacySearchService.searchPharmacyDtoList()
+                .stream().map(pharmacyDto ->
+                        Direction.builder()
+                                .inputAddress(documentDto.getAddressName())
+                                .inputLatitude(documentDto.getLatitude())
+                                .inputLongitude(documentDto.getLongitude())
+                                .targetPharmacyName(pharmacyDto.getPharmacyName())
+                                .targetAddress(pharmacyDto.getPharmacyAddress())
+                                .targetLatitude(pharmacyDto.getLatitude())
+                                .targetLongitude(pharmacyDto.getLongitude())
+                                .distance(
+                                        calculateDistance(documentDto.getLatitude(), documentDto.getLongitude(),
+                                                pharmacyDto.getLatitude(), pharmacyDto.getLongitude()))
+                                .build())
+                .filter(direction -> direction.getDistance() <= RADIUS_KM)
+                .sorted(Comparator.comparing(Direction::getDistance))
+                .limit(MAX_SEARCH_COUNT)
+                .collect(Collectors.toList());
+    }
+
+    // Haversine formula
+    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        lat1 = Math.toRadians(lat1);
+        lon1 = Math.toRadians(lon1);
+        lat2 = Math.toRadians(lat2);
+        lon2 = Math.toRadians(lon2);
+
+        double earthRadius = 6371; //Kilometers
+        return earthRadius * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+    }
+
+}
